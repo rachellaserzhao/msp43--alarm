ce2b0492e13dc718229d78d5a5def87c17b53d1e
rachellaserzhao <ruiqirachelzhao@gmail.com>
2015-09-22 22:17:38 +0200

    added a file

---
+++ cpractice.c
@@ -0,0 +1,20 @@
+#include <studio.h>
+unsigned int x 
+unsigned int y 
+
+int main void (){
+
+x = 5; 
+y = 6; 
+
+x = x + y ; 
+y = x - y; 
+x = x - y; 
+printf ("the values are %d  %d", x y);
+
+return 0;
+
+
+
+
+}
